f9aec8dca27331a1ba9eef4baafc2246f26c64bc
unknown <keith.francis83@gmail.com>
2026-02-25 19:02:17 -0700

    added ability to delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -51,6 +51,25 @@ export const removeLike = postId => async dispatch => {
         likes: res.data
       }
     });
+  } catch(err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete Post
+export const deletePost = postId => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId
+    });
+
+    dispatch(setAlert('Post Removed', 'Success'));
   } catch(err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ auth, addLike, removeLike, post: { 
+const PostItem = ({ auth, addLike, removeLike, deletePost, post: { 
     _id, 
     text, 
     name, 
@@ -45,7 +45,7 @@ const PostItem = ({ auth, addLike, removeLike, post: {
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type="button" class="btn btn-danger">
+          <button onClick={e => deletePost(_id)} type="button" class="btn btn-danger">
             <i class="fas fa-times"></i>
           </button>
         )}
@@ -56,11 +56,14 @@ const PostItem = ({ auth, addLike, removeLike, post: {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {addLike, removeLike})(PostItem)
+export default connect(mapStateToProps, {addLike, removeLike, deletePost})(PostItem)
